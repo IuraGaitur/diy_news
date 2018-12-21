3604723b02e14d82d84d54345906e19af8a40cce
Iura <iura.gaitur@gmail.com>
2018-12-21 18:13:05 +0200

    Added changes in tutorial

---
+++ app/src/main/java/video/paxra/com/videoconverter/fragments/TutorialFragment.java
@@ -0,0 +1,31 @@
+package video.paxra.com.videoconverter.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import video.paxra.com.videoconverter.R;
+
+public class TutorialFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tutorial, null);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViewDetails();
+    }
+
+    private void initViewDetails() {
+
+    }
+}
